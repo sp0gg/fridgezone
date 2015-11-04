05cafe048ae3fab460eb854e5d272b7069e12972
sp0gg <joe.nelson@runbox.com>
2015-11-04 18:02:01 -0500

    remove repo reference

--- src/main/java/net/sp0gg/fridgezone/web/FridgezoneController.java
+++ src/main/java/net/sp0gg/fridgezone/web/FridgezoneController.java
@@ -1,7 +1,5 @@
 package net.sp0gg.fridgezone.web;
 
-import net.sp0gg.fridgezone.data.repository.ItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,13 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class FridgezoneController {
 	
-	private ItemRepository repo;
-	
-	@Autowired
-	public FridgezoneController(ItemRepository repo) {
-		this.repo = repo;
-	}
-
     @RequestMapping(value = "/", method=RequestMethod.GET)
 	public String root(){
 		return "redirect:/inventory";
@@ -25,5 +16,4 @@ public class FridgezoneController {
     public String inventory(){
         return "inventory";
     }
-
 }
