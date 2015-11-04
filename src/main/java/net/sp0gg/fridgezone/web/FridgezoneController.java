ad615ff3dd89409bca5de5ed17fd9c4dbb70db97
sp0gg <joe.nelson@runbox.com>
2015-11-03 19:34:12 -0500

    removed unnecessary repo call from web controller

--- src/main/java/net/sp0gg/fridgezone/web/FridgezoneController.java
+++ src/main/java/net/sp0gg/fridgezone/web/FridgezoneController.java
@@ -1,14 +1,10 @@
 package net.sp0gg.fridgezone.web;
 
 import net.sp0gg.fridgezone.data.repository.ItemRepository;
-import net.sp0gg.fridgezone.domain.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @Controller
 public class FridgezoneController {
@@ -26,9 +22,7 @@ public class FridgezoneController {
 	}
 
     @RequestMapping(value = "/inventory", method=RequestMethod.GET)
-    public String inventory(Model model){
-        List<Item> items = repo.findAll();
-        model.addAttribute("items", items);
+    public String inventory(){
         return "inventory";
     }
 
